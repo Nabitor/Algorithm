85fb18d8cd1670f746723763cae52998327eda0d
Nabitor <xj174850@163.com>
2017-10-22 15:48:10 +0800

    add merge_two_binary_tree.py

---
+++ Algorithm/Math/hamming_distance.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2017-10-22 15:34:18
+# @Author  : xuejun (xuemyjun@gmail.com)
+
+"""
+The Hamming distance between two integers is the number of positions
+at which the corresponding bits are different.
+
+Given two integers x and y, calculate the Hamming distance.
+
+Note:
+0 ≤ x, y < 231.
+
+Example:
+
+Input: x = 1, y = 4
+
+Output: 2
+
+Explanation:
+1   (0 0 0 1)
+4   (0 1 0 0)
+       ↑   ↑
+
+The above arrows point to positions where the corresponding bits are different.
+"""
+
+
+class Solution(object):
+    def hammingDistance(self, x, y):
+        """
+        :type x: int
+        :type y: int
+        :rtype: int
+        """
+        return bin(x ^ y).count("1")
+
+
+if __name__ == '__main__':
+    solution = Solution()
+    print(solution.hammingDistance(1, 4))
